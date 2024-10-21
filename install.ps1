40886e94a1f02d878cf883caa3c6955cdc180457
Javi Hache <hermosa@krater.io>
2024-10-22 00:39:51 +0200

    Changes

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 $curDir = Get-Location
 $pm2InstallerFolder = "C:\Program Files\pm2-installer"
 $pm2InstallerZipfile = "$HOME\Downloads\pm2-installer.zip"
-
+$installationFolder = "C:\Program Files\Braiins-Manager-Agent"
 # Define text color variables
 # $RED = "Red"
 # $GREEN = "Green"
@@ -58,12 +58,19 @@ Braiins is not responsible for the undesired changes this script might do to you
 
 Please review and understand this script before executing it and if you are not sure what it does, please get in touch with us by sending an e-mail to help@braiins.com.
 
-Please confirm that you understand this script and you wish to proceed."
+Please confirm that you understand this script and you wish to proceed.
+
+"
     $choices = '&Yes', '&No'
     $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
     if ($decision -eq 0)
     {
         Info "Proceeding with installation"
+        # Get valid name and ID from the user
+        $name = Get-ValidName
+        $ID = Get-ValidID
+        $nameLine = "name='$name'"
+        $IDLine = "id='$ID'"
         InstallBraiinsManagerAgent
     }
     else
@@ -74,6 +81,69 @@ Please confirm that you understand this script and you wish to proceed."
     }
 }
 
+
+# Function to validate name input
+function Get-ValidName() {
+    while ($true) {
+        Info "Braiins Manager Agent instance name (e.g., location, data center, or any unique name): "
+        $name = Read-Host
+        if (-not [string]::IsNullOrWhiteSpace($name)) {
+            return $name
+        } else {
+            Warning "Instance name cannot be empty."
+        }
+    }
+}
+
+# Function to validate ID input against the UUID regex pattern
+function Get-ValidID() {
+    $regex = '^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$'
+    while ($true) {
+        Info "Enter your Braiins Manager Agent ID (UUID Format): "
+        $ID = Read-Host 
+        if (-not [string]::IsNullOrWhiteSpace($ID) -and $ID -match $regex) {
+            return $ID
+        } else {
+            Warning "Invalid Agent ID. Value does not match the UUID format."
+        }
+    }
+}
+
+function Create_Env_File() {
+    cd $installationFolder
+    $outputFile = "$installationFolder\.env.config"
+    Set-Content -Path $outputFile -Value "$nameLine`r`n$IDLine"
+}
+
+# Write the validated inputs to the file in the specified format
+function Install_NodeJS {
+
+    if (-NOT (Get-Command node -ErrorAction SilentlyContinue)) {
+          #Install Winget if not present
+        if ((Get-Command winget -ErrorAction SilentlyContinue)) { 
+            try {
+                Info "Attempting to install NodeJS using Winget"
+                winget install -h OpenJS.NodeJS.LTS
+            }
+            catch {
+                Debug "Winget is not installed in this Windows system. Trying to install NodeJS with MSI packages."
+                $msiUrl = "https://nodejs.org/dist/v20.18.0/node-v20.18.0-x64.msi"
+                # Remember to update this hardcoded link to the next major version of NodeJS in the future.
+                $localMsiPath = "$env:TEMP\node-v20.18.0-x64.msi"
+                Invoke-WebRequest -Uri $msiUrl -OutFile $localMsiPath
+                Start-Process msiexec.exe -ArgumentList "/i `"$localMsiPath`" /quiet /norestart" -Wait
+                Remove-Item $localMsiPath
+            }
+            finally {
+                Info "NodeJS succesfully installed! Proceeding..."
+            }
+        }
+    else {
+        Info "NodeJS is already installed! Proceeding..."
+    }     
+    }
+}
+
 function InstallBraiinsManagerAgent()
 {
     $start_time = Get-Date
@@ -81,67 +151,59 @@ function InstallBraiinsManagerAgent()
     # Check for admin rights
     if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
     {
-        write-Warning "This setup needs Administrator permissions. Please run this installer on a Powershell as Administrator."
-        Start-Sleep -Seconds 1
-        exit
+        Write-Warning "This setup needs Administrator permissions. Please run this installer on a Powershell as Administrator."
+        exit 1
     }
-
-    #Install Winget if not present
-    if (-NOT (Get-Command winget -ErrorAction SilentlyContinue))
-    {
-        Install-WinGet
-        $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-    }
-
-    #Install NodeJS and PM2
-    if (-NOT (Get-Command node -ErrorAction SilentlyContinue))
-    {
-        winget install -h OpenJS.NodeJS.LTS
-    }
-    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-    npm install -global pm2
-
-    #Install PM2-installer by jessety
-    CheckPM2Installer
-    pm2 update
-
     #Select folder and install the files
-    while ($true)
-    {
-        # Prompt the user for the installation folder
-        $installationFolder = Select-Folder
-
-        # Break the loop if the user pressed Cancel in the folder dialog
-        if ($null -eq $installationFolder)
-        {
-            Write-Host "Installation canceled by the user."
-            exit
-        }
-
-        # Ensure the folder exists before checking contents
-        if (Test-Path -Path $installationFolder)
-        {
-
-            # Use [IO.Directory]::GetFiles() to check for files in the folder
-            $filesInFolder = [System.IO.Directory]::GetFiles($installationFolder)
-            if ($filesInFolder.Count -eq 0)
+    if (-NOT Test-Path -Path $installationFolder) {
+        Info "Braiins Manager Agent will be installed in $installationFolder"
+    } else {
+        while ($true) {
+            # Prompt the user for the installation folder
+            $installationFolder = Select-Folder
+            # Break the loop if the user pressed Cancel in the folder dialog
+            if ($null -eq $installationFolder)
             {
-                Write-Host "The folder is empty. Proceeding with installation."
-                break
+                Write-Host "Installation canceled by the user."
+                exit
+            }
+
+            # Ensure the folder exists before checking contents
+            if (Test-Path -Path $installationFolder)
+            {
+
+                # Use [IO.Directory]::GetFiles() to check for files in the folder
+                $filesInFolder = [System.IO.Directory]::GetFiles($installationFolder)
+                if ($filesInFolder.Count -eq 0)
+                {
+                    Write-Host "$installationFolder is empty. Proceeding with installation."
+                    break
+                }
+                else
+                {
+                    Write-Host "$installationFolder not empty. Please choose an empty folder to install."
+                }
             }
             else
             {
-                Write-Host "The folder is not empty. Please choose an empty folder to install."
+                Write-Host "The selected folder does not exist. Please choose a valid folder."
             }
         }
-        else
-        {
-            Write-Host "The selected folder does not exist. Please choose a valid folder."
-        }
+
     }
+    #Install NodeJS and PM2
+    Install_NodeJS
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+    if (-NOT (Get-Command pm2 -ErrorAction SilentlyContinue)) {
+        npm install -global pm2
+    }   
+    #Install PM2-installer by jessety
+    CheckPM2Installer
+    pm2 update
+    
     # Proceed with the rest of the script
     Write-Host "Proceeding with installation in folder: $installationFolder"
-    cd $installationFolder
+    Create_Env_File
     $distZipfile = Join-Path -Path $installationFolder -ChildPath krater-node.zip
     Invoke-WebRequest -Uri https://braiinspublic.blob.core.windows.net/agent/dist.zip  -OutFile $distZipfile
     Expand-Archive -path $distZipfile -DestinationPath $installationFolder -Force
@@ -151,92 +213,17 @@ function InstallBraiinsManagerAgent()
     Invoke-WebRequest -Uri https://braiinspublic.blob.core.windows.net/agent/onboarding.js -OutFile $installationFolder\onboarding.js
     Invoke-WebRequest -Uri https://braiinspublic.blob.core.windows.net/agent/pm2.config.js -OutFile $installationFolder\pm2.config.js
     Start-Process -FilePath "npm" -ArgumentList "--silent ci" -WorkingDirectory $installationFolder -Wait -NoNewWindow
-    New-Item -Path $installationFolder\.env.config -ItemType File | Out-Null
-    cd $installationFolder
-    node $installationFolder\onboarding.js
     pm2 -s start pm2.config.js --time --exp-backoff-restart-delay=3000
     pm2 -s save
     cd $curDir
-    write-Host "
+    Info "
 Installation completed in: $( (Get-Date).Subtract($start_time).Seconds ) seconds.
 
 Braiins Manager Agent has been successfully installed and will start on boot!
 
 Please use command 'pm2' and the corresponding arguments to start, stop and restart Braiins Manager Agent. 
-You can do 'pm2 --help' to obtain more information"-ForegroundColor Green
+You can do 'pm2 --help' to obtain more information"
 
-}
-
-Function Install-WinGet
-{
-    #Install the latest package from GitHub
-    [cmdletbinding(SupportsShouldProcess)]
-    [alias("iwg")]
-    [OutputType("None")]
-    [OutputType("Microsoft.Windows.Appx.PackageManager.Commands.AppxPackage")]
-    Param(
-        [Parameter(HelpMessage = "Display the AppxPackage after installation.")]
-        [switch]$Passthru
-    )
-
-    Write-Verbose "[$( (Get-Date).TimeofDay )] Starting $( $myinvocation.mycommand )"
-
-    if ($PSVersionTable.PSVersion.Major -eq 7)
-    {
-        Write-Warning "This command does not work in PowerShell 7. You must install in Windows PowerShell."
-        return
-    }
-
-    #test for requirement
-    $Requirement = Get-AppPackage "Microsoft.DesktopAppInstaller"
-    if (-Not $requirement)
-    {
-        Write-Verbose "Installing Desktop App Installer requirement"
-        Try
-        {
-            Add-AppxPackage -Path "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx" -erroraction Stop
-        }
-        Catch
-        {
-            Throw $_
-        }
-    }
-
-    $uri = "https://api.github.com/repos/microsoft/winget-cli/releases"
-
-    Try
-    {
-        Write-Verbose "[$( (Get-Date).TimeofDay )] Getting information from $uri"
-        $get = Invoke-RestMethod -uri $uri -Method Get -ErrorAction stop
-
-        Write-Verbose "[$( (Get-Date).TimeofDay )] getting latest release"
-        #$data = $get | Select-Object -first 1
-        $data = $get[0].assets | Where-Object name -Match 'msixbundle'
-        $appx = $data.browser_download_url
-        #$data.assets[0].browser_download_url
-        Write-Verbose "[$( (Get-Date).TimeofDay )] $appx"
-        If ( $pscmdlet.ShouldProcess($appx, "Downloading asset"))
-        {
-            $file = Join-Path -path $env:temp -ChildPath $data.name
-
-            Write-Verbose "[$( (Get-Date).TimeofDay )] Saving to $file"
-            Invoke-WebRequest -Uri $appx -UseBasicParsing -DisableKeepAlive -OutFile $file
-
-            Write-Verbose "[$( (Get-Date).TimeofDay )] Adding Appx Package"
-            Add-AppxPackage -Path $file -ErrorAction Stop
-
-            if ($passthru)
-            {
-                Get-AppxPackage microsoft.desktopAppInstaller
-            }
-        }
-    } #Try
-    Catch
-    {
-        Write-Verbose "[$( (Get-Date).TimeofDay )] There was an error."
-        Throw $_
-    }
-    Write-Verbose "[$( (Get-Date).TimeofDay )] Ending $( $myinvocation.mycommand )"
 }
 
 function Select-Folder
